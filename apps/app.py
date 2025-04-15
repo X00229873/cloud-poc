ca09c5b59c462790f5009bdfc468d2e5c6ed6292
X00229873 <X00229873@myTUDublin.ie>
2025-04-15 17:18:52 +0100

    No Script

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Flask on AWS EC2!"
+def home():
+    return "✅ Flask app running on Ubuntu EC2!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
